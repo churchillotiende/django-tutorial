993d257f5164ab234f9d321561e7f73e88c34717
churchillotiende <churchillotiende84@gmail.com>
2024-10-13 01:10:01 +0300

    Successfully established postgresql connection

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Film(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(max_length=400)
+    pub_date = models.DateTimeField("date published")
+
+
+class Gener(models.Model):
+    film_id = models.ForeignKey(Film, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)   
